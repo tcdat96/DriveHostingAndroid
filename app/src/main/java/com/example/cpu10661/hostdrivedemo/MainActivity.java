51f9dd16fb4e60debe4acd3a99b42a14e044f8c9
dcvh <dungchoviechoc@gmail.com>
2018-02-06 17:17:24 +0700

    initial commit

---
+++ app/src/main/java/com/example/cpu10661/hostdrivedemo/MainActivity.java
@@ -0,0 +1,299 @@
+package com.example.cpu10661.hostdrivedemo;
+
+import android.accounts.Account;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.drive.DriveClient;
+import com.google.android.gms.drive.DriveResourceClient;
+import com.google.android.gms.tasks.Task;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.example.cpu10661.hostdrivedemo.Utils.DriveApiUtils;
+
+import static com.google.android.gms.drive.Drive.SCOPE_APPFOLDER;
+import static com.google.android.gms.drive.Drive.SCOPE_FILE;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int REQUEST_CODE_SIGN_IN = 1;
+    private static final int REQUEST_AUTHORIZATION = 2;
+
+    // Handles high-level drive functions like sync
+    private DriveClient mDriveClient;
+    // Handle access to Drive resources/files.
+    private DriveResourceClient mDriveResourceClient;
+    private GoogleAccountCredential mCredential;
+    private Drive mService;
+    private String mSharedFolderId = null;
+
+    private Handler mHandler;
+    private ArrayAdapter<String> mAdapter;
+    private ArrayList<String> mNameList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        HandlerThread handlerThread = new HandlerThread(getPackageName());
+        handlerThread.start();
+        mHandler = new Handler(handlerThread.getLooper());
+
+        initUiComponents();
+    }
+
+    private void initUiComponents() {
+        // add demo button
+        Button addDemoButton = findViewById(R.id.btn_add_demo);
+        addDemoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        saveDemoFile();
+                    }
+                });
+            }
+        });
+
+        // file list view
+        ListView nameListView = findViewById(R.id.lv_shared_with_me_files);
+        mNameList = new ArrayList<>();
+        mAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, mNameList);
+        nameListView.setAdapter(mAdapter);
+        nameListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final String fileName = mNameList.get(position);
+                showFileContent(fileName);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        signIn();
+    }
+
+    /**
+     * Starts the sign-in process and initializes the Drive client.
+     */
+    protected void signIn() {
+        Set<Scope> requiredScopes = new HashSet<>(2);
+        requiredScopes.add(SCOPE_FILE);
+        requiredScopes.add(SCOPE_APPFOLDER);
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if (signInAccount != null && signInAccount.getGrantedScopes().containsAll(requiredScopes)) {
+            initializeDriveClient(signInAccount);
+        } else {
+            GoogleSignInOptions signInOptions =
+                    new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                            .requestScopes(SCOPE_FILE)
+                            .requestScopes(SCOPE_APPFOLDER)
+                            .requestEmail()
+                            .build();
+            GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(this, signInOptions);
+            startActivityForResult(googleSignInClient.getSignInIntent(), REQUEST_CODE_SIGN_IN);
+        }
+    }
+
+    /**
+     * Continues the sign-in process, initializing the Drive clients with the current
+     * user's account.
+     */
+    private void initializeDriveClient(GoogleSignInAccount signInAccount) {
+        mDriveClient = com.google.android.gms.drive.Drive.getDriveClient(
+                getApplicationContext(), signInAccount);
+        mDriveResourceClient = com.google.android.gms.drive.Drive.getDriveResourceClient(
+                getApplicationContext(), signInAccount);
+
+        mCredential = GoogleAccountCredential.usingOAuth2(this,
+                Collections.singletonList(DriveScopes.DRIVE));
+        if (signInAccount.getAccount() != null) {
+            String name = signInAccount.getAccount().name;
+            String type = signInAccount.getAccount().type;
+            mCredential.setSelectedAccount(new Account(name, type));
+        }
+        mService = getDriveService(mCredential);
+
+        onDriveClientReady();
+    }
+
+    private Drive getDriveService(GoogleAccountCredential credential) {
+        return new Drive
+                .Builder(AndroidHttp.newCompatibleTransport(), new GsonFactory(), credential)
+                .build();
+    }
+
+    private void onDriveClientReady() {
+        showSharedWithMeFileList();
+    }
+
+    /**
+     * this method looked up specified file, reads its content and show it in an OK dialog
+     * this method will be executed asynchronously
+     *
+     * @param fileName name of specified file
+     */
+    private void showFileContent(final String fileName) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                String fileId = DriveApiUtils.getFileId(mService, mSharedFolderId, fileName);
+                if (!fileId.isEmpty()) {
+                    String content = DriveApiUtils.readFileContent(mService, fileId);
+                    Utils.showOkDialog(MainActivity.this, fileName, content);
+                }
+            }
+        });
+    }
+
+    private void showSharedWithMeFileList() {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mSharedFolderId = DriveApiUtils.getSharedWithMeFolder(
+                        mService, mCredential.getSelectedAccountName());
+                final String[] nameList = DriveApiUtils.getSharedWithMeFileList(mService, mSharedFolderId);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mNameList.clear();
+                        mNameList.addAll(Arrays.asList(nameList));
+                        mAdapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        });
+    }
+
+    /**
+     * this method is synchronous and must be called in background thread
+     */
+    private void saveDemoFile() {
+        final String fileName = Utils.getCurrentTimestamp();
+        java.io.File demoFile = createDemoFile(fileName);
+
+        if (demoFile != null) {
+            File fileMetadata = new File();
+            fileMetadata.setName(fileName);
+            fileMetadata.setParents(Collections.singletonList(mSharedFolderId));
+            FileContent mediaContent = new FileContent("text/plain", demoFile);
+            try {
+                final File file = mService.files().create(fileMetadata, mediaContent)
+                        .setFields("id, parents")
+                        .execute();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mNameList.add(fileName);
+                        mAdapter.notifyDataSetChanged();
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Nullable
+    private java.io.File createDemoFile(String fileName) {
+        if (!isExternalStorageWritable()) {
+            Toast.makeText(this, "External storage is not writable", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+
+        java.io.File file = new java.io.File(getExternalFilesDir(null), fileName);
+        try {
+            String content = getPackageName() + "\n"
+                    + mCredential.getSelectedAccountName() + "\n"
+                    + Utils.getCurrentTimestamp();
+            FileOutputStream outputStream = new FileOutputStream(file);
+            outputStream.write(content.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+    private boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CODE_SIGN_IN:
+                if (resultCode != RESULT_OK) {
+                    Log.e(TAG, "Sign-in failed.");
+                    finish();
+                    return;
+                }
+
+                Task<GoogleSignInAccount> getAccountTask =
+                        GoogleSignIn.getSignedInAccountFromIntent(data);
+                if (getAccountTask.isSuccessful()) {
+                    initializeDriveClient(getAccountTask.getResult());
+                } else {
+                    Log.e(TAG, "Sign-in failed.");
+                    finish();
+                }
+                break;
+            case REQUEST_AUTHORIZATION:
+                if (resultCode == Activity.RESULT_OK) {
+                    showSharedWithMeFileList();
+                } else {
+                    signIn();
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ app/src/main/java/com/example/cpu10661/hostdrivedemo/Utils.java
@@ -0,0 +1,138 @@
+package com.example.cpu10661.hostdrivedemo;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.annotation.Nonnull;
+
+class Utils {
+    static String getCurrentTimestamp() {
+        return new SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault()).format(new Date());
+    }
+
+    static void showOkDialog(Context context, String title, String message) {
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(context, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(context);
+        }
+        builder.setTitle(title)
+                .setMessage(message)
+                .setNeutralButton(android.R.string.ok, null)
+                .show();
+    }
+
+    static class DriveApiUtils {
+        /**
+         * this method is synchronous and must be called in background thread
+         *
+         * @return the destination folder ID
+         */
+        @Nonnull
+        static String getSharedWithMeFolder(Drive service, String email) {
+            String folderId = "";
+            FileList fileList = getFileList(service, "sharedWithMe = true");
+            for (File file : fileList.getFiles()){
+                if (file.getName().equals(email)) {
+                    folderId = file.getId();
+                    break;
+                }
+            }
+            return folderId;
+        }
+
+        /**
+         * this method is synchronous and must be called in background thread
+         *
+         * @param folderId the specified folder's ID
+         * @return a list of specified folder's children files
+         */
+        static String[] getSharedWithMeFileList(Drive service, String folderId) {
+            String q = String.format("'%s' in parents", folderId);
+            FileList fileList = getFileList(service, q);
+
+            int totalFiles = fileList.getFiles().size();
+            String[] nameList = new String[totalFiles];
+            for (int i = 0; i < totalFiles; i++) {
+                nameList[i] = fileList.getFiles().get(i).getName();
+            }
+            return nameList;
+        }
+
+        @Nonnull
+        static String getFileId(Drive service, String folderId, String fileName) {
+            String q = String.format("'%s' in parents and name='%s'", folderId, fileName);
+            FileList fileList = getFileList(service, q);
+            if (fileList != null && !fileList.getFiles().isEmpty()) {
+                return fileList.getFiles().get(0).getId();
+            }
+            return "";
+        }
+
+
+        /**
+         * this method is synchronous and must be called in background thread
+         *
+         * @param q query string
+         * @return FileList result of specified query string
+         */
+        private static FileList getFileList(Drive service, String q) {
+            FileList fileList = null;
+            try {
+                Drive.Files.List list = service.files().list().setSpaces("drive");
+                if (q != null && q.length() > 0) {
+                    list.setQ(q);
+                }
+                fileList = list.execute();
+            } catch (final UserRecoverableAuthIOException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return fileList;
+        }
+
+        static String readFileContent(Drive service, String fileId) {
+            try {
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                service.files().get(fileId).executeMediaAndDownloadTo(outputStream);
+                InputStream is = new ByteArrayInputStream(outputStream.toByteArray());
+                return convertInputStreamToString(is);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        private static String convertInputStreamToString(InputStream inputStream) {
+            try {
+                ByteArrayOutputStream result = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = inputStream.read(buffer)) != -1) {
+                    result.write(buffer, 0, length);
+                }
+                return result.toString("UTF-8");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+}
